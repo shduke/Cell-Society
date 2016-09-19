193d664e50dbea57bbb51dd67483facb6d441b3d
Michael Schroeder <ms548@duke.edu>
2016-09-18 23:22:59 -0400

    add Simulation superclass and subclasses

---
+++ src/simulation/GameOfLifeSimulation.java
@@ -0,0 +1,11 @@
+package simulation;
+
+public class GameOfLifeSimulation extends Simulation {
+
+    @Override
+    public void step () {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ src/simulation/PredatorPreySimulation.java
@@ -0,0 +1,11 @@
+package simulation;
+
+public class PredatorPreySimulation extends Simulation {
+
+    @Override
+    public void step () {
+        // TODO Auto-generated method stub
+
+    }
+
+}
