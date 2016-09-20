9674348d642fb3749ef1c84f3be19eca7cba4be0
Michael Schroeder <ms548@duke.edu>
2016-09-19 23:11:36 -0400

    add predatorprey instance variables

--- src/simulation/PredatorPreySimulation.java
+++ src/simulation/PredatorPreySimulation.java
@@ -1,7 +1,16 @@
 package simulation;
 
+import grid.*;
+
 public class PredatorPreySimulation extends Simulation {
 
+    private int myPreyBreedTime;
+    private int myPredatorBreedTime;
+    private int myFishEnergy;
+    private Grid myGrid;
+    
+    
+    
     @Override
     public void step () {
         // TODO Auto-generated method stub
