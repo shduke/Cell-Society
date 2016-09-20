5331b0bea2447fe4816d65a1c8ae17b24db205d9
Kayla Schulz <kms114@duke.edu>
2016-09-19 23:28:04 -0400

    Added buttons and sliders to toolbar, created scene to start application

--- src/applicationView/Toolbar.java
+++ src/applicationView/Toolbar.java
@@ -1,5 +1,39 @@
 package applicationView;
 
-public class Toolbar {
+import javafx.collections.FXCollections;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.HBox;
 
+public class Toolbar {
+    
+    private int toolbarSize;
+    Button pause;
+    Button step;
+    HBox myToolbar;
+    
+    protected void initToolbar(int height, int width, Scene myScene, Group root) {
+        myScene.setRoot(root);
+        myToolbar = new HBox(20.0);
+        Slider slider = new Slider(0, 1, 0.5);
+        pause = new Button("Pause");
+        step = new Button("Step");
+        ChoiceBox cb = new ChoiceBox(FXCollections.observableArrayList(
+                                                                       "Fire", "Game of Life", "Predator-Prey", "Segregation")
+                                                                   );
+        myToolbar.getChildren().addAll(slider, pause, step, cb);
+        addToRoot(root);
+    }
+    
+    private void addToRoot(Group root) {
+        root.getChildren().add(myToolbar);
+    }
+    
+    private void pauseSimulation() {
+        
+    }
+    
 }
---
+++ src/applicationView/createScene.java
@@ -0,0 +1,36 @@
+package applicationView;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class createScene extends Application {
+    
+    public static final int FRAMES_PER_SECOND = 60;
+    private static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
+    private static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
+    private static final int SCREEN_SIZE = 400;
+    private Toolbar myToolbar;
+    private Group root = new Group();
+    
+    @Override
+    public void start(Stage s) {
+        myToolbar = new Toolbar();
+        Scene scene = new Scene(root);
+        myToolbar.initToolbar(SCREEN_SIZE, SCREEN_SIZE, scene, root);
+
+        s.setScene(scene);
+        s.show();
+    }
+    
+    public static void main (String[] args) {
+        launch(args);
+    }
+
+    
+}
