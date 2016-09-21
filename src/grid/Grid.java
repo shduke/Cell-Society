95846e036a01f58e570dbad5daf57635fa999a5f
Michael Schroeder <ms548@duke.edu>
2016-09-20 23:05:41 -0400

    update basic grid methods

--- src/grid/Grid.java
+++ src/grid/Grid.java
@@ -1,5 +1,21 @@
 package grid;
 
+import cell.*;
+
 public abstract class Grid {
 
+    Cell[][] myCells;
+    
+    public Cell[][] getMyCells() {
+        return myCells;
+    }
+    
+    public int getNumRows() {
+        return myCells.length;
+    }
+    
+    public int getNumColumns() {
+        return myCells[0].length;
+    }
+    
 }
