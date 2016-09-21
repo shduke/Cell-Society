12ccd4a6baedd0f87c5f8a31e67d87310ddbfd65
Sean Hudson <srh50@duke.edu>
2016-09-21 04:33:51 -0400

    added some xml template functions

--- src/xml/XMLParser.java
+++ src/xml/XMLParser.java
@@ -1,5 +1,22 @@
 package xml;
 
-public class XMLReader {
+import java.util.ArrayList;
+import java.util.Collection;
+import cell.Cell;
+import simulation.FireSimulation;
+import simulation.Simulation;
 
+
+/// Responsible for building the simulation
+public class XMLParser {
+
+    public Collection<Cell> getInitCells () {
+        // create cells of the specified cell type (cell type also read in from XML)
+        return new ArrayList<Cell>();
+    }
+    
+    //filler code
+    public Simulation buildSimulation() {
+        return new FireSimulation();
+    }
 }
