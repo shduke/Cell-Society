5cf05ff2d898a86de7f032553f6692a7df65d356
Sean Hudson <srh50@duke.edu>
2016-09-22 11:36:22 -0400

    added enough code for to create a tester simulation

---
+++ src/main/Main.java
@@ -0,0 +1,50 @@
+package main;
+
+import controller.ApplicationController;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+
+/**
+ * Main class for creating an animated scene.
+ * 
+ */
+public class Main extends Application {
+    public static final int SIZE = 500;
+    public static final int FRAMES_PER_SECOND = 60;
+    private static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
+    //private static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
+    private static final double SECOND_DELAY = 2.0;
+
+
+    private ApplicationController applicationController;
+
+    /**
+     * Sets the Game up at the beginning.
+     * 
+     * @return nothing
+     */
+    @Override
+    public void start (Stage s) {
+        applicationController = new ApplicationController();
+        s.setTitle(applicationController.getTitle());
+
+        Scene scene = applicationController.init(SIZE, SIZE);
+        s.setScene(scene);
+        s.show();
+
+        KeyFrame frame = new KeyFrame(Duration.millis(SECOND_DELAY),
+                                      e -> applicationController.getSimulationController().getSimulation().step());
+        Timeline animation = new Timeline();
+        animation.setCycleCount(Timeline.INDEFINITE);
+        animation.getKeyFrames().add(frame);
+        animation.play();
+    }
+    public static void main (String[] args) {
+        launch(args);
+    }
+}
--- src/simulation/FireSimulation.java
+++ src/simulation/FireSimulation.java
@@ -1,11 +1,83 @@
 package simulation;
 
-public class FireSimulation extends Simulation {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import cell.Cell;
+import cell.FireCell;
+import cell.State;
+import grid.Coordinate;
+import grid.GridView;
+import grid.Neighbors;
+import grid.RectangleGrid;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.geometry.Dimension2D;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
 
+
+public class FireSimulation extends Simulation {
+    Map<Coordinate, Cell> temp = new HashMap<Coordinate, Cell>(); //temporary solution
+
+    
+    public FireSimulation () {
+        ArrayList<Cell> input = new ArrayList<Cell>();
+
+        input.add(new FireCell(State.BURNING,
+                               new Coordinate(2, 2)));
+        setGrid(new RectangleGrid(5, 5, input));
+        setGridView(new GridView(new Dimension2D(500, 500), "Rectangle", getGrid()));
+    }
+
+    //very temporary code, just here to make it work
     @Override
     public void step () {
-        // TODO Auto-generated method stub
+        Iterator<Map.Entry<Coordinate, Cell>> iterCell = getGrid().getCellGridIterator();
+        while (iterCell.hasNext()) {
+            Cell cell = iterCell.next().getValue();
+            createNeighbors(cell);
 
+        }
+        for (Coordinate coordinate : temp.keySet()) {
+            getGrid().addCell(temp.get(coordinate));
+        }
+        temp.clear();
+        iterCell = getGrid().getCellGridIterator();
+        while (iterCell.hasNext()) {
+            Cell cell = iterCell.next().getValue();
+            setNextState(cell);
+        }
+        getGrid().updateGrid();
+    }
+
+    @Override
+    public void createNeighbors (Cell cell) {
+        Neighbors neighbors = getGrid().getNeighbors(cell);
+        Iterator<Coordinate> iterNewCell = neighbors.getUncreatedNeighborCoordinates();
+        while (iterNewCell.hasNext()) {
+            Coordinate coordinate = iterNewCell.next();
+            Cell newCell = new FireCell(State.EMPTY, coordinate);
+            //getGrid().addCell(newCell);
+            temp.put(coordinate, newCell);
+        }
+    }
+
+    // test filler code
+    @Override
+    public void setNextState (Cell cell) {
+        if (cell.getMyCurrentState() == State.EMPTY) {
+            cell.setMyNextState(State.TREE);
+        }
+        else if (cell.getMyCurrentState() == State.TREE) {
+            cell.setMyNextState(State.BURNING);
+        }
+        else {
+            cell.setMyNextState(State.EMPTY);
+        }
     }
 
 }
