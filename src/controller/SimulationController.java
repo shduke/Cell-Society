6810129f5dec1cace0a3588a2feec566d4d6a89f
Michael Schroeder <ms548@duke.edu>
2016-09-23 16:53:42 -0400

    predatorprey testing

--- src/controller/SimulationController.java
+++ src/controller/SimulationController.java
@@ -5,7 +5,7 @@ import javafx.scene.Group;
 import simulation.FireSimulation;
 import simulation.Simulation;
 import xml.XMLParser;
-
+import simulation.*;
 
 public class SimulationController {
 
@@ -16,7 +16,8 @@ public class SimulationController {
 
     SimulationController(Group simulationRoot) {
         this.simulationRoot = simulationRoot;
-        simulation = new FireSimulation();
+        //simulation = new FireSimulation();
+        simulation = new PredatorPreySimulation();
         simulation.addGridViewSceneGraph(simulationRoot);
     }
     private void initializeSimulation () {
@@ -24,7 +25,12 @@ public class SimulationController {
     }
 
     //for testing
+    /*
     public FireSimulation getSimulation() {
         return (FireSimulation)simulation;
+    }*/
+    
+    public PredatorPreySimulation getSimulation() {
+        return (PredatorPreySimulation)simulation;
     }
 }
