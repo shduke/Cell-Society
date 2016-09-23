8e2898c0ba7ec7678228dad577e6c00c225c6dd5
Michael Schroeder <ms548@duke.edu>
2016-09-23 19:16:15 -0400

    update loop timing

--- src/main/Main.java
+++ src/main/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
     public static final int FRAMES_PER_SECOND = 1;
     private static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
     //private static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
-    private static final double SECOND_DELAY = 2.0;
+    private static final double SECOND_DELAY = 100.0;
 
 
     private ApplicationController applicationController;
