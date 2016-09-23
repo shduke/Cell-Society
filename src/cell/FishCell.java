38b296bbacf22ee2c5342f03024435d74dae90dd
Michael Schroeder <ms548@duke.edu>
2016-09-23 19:15:17 -0400

    add constructor with coordinate

--- src/cell/FishCell.java
+++ src/cell/FishCell.java
@@ -15,14 +15,18 @@ public class FishCell extends Cell {
         myMaxBreedTime = breedTime;
         myCurrentBreedTime = myMaxBreedTime;
     }
-
+    public FishCell(Coordinate coord, int breedTime) {
+        super(State.FISH, coord);
+        myMaxBreedTime = breedTime;
+        myCurrentBreedTime = myMaxBreedTime;
+    }
     public FishCell(FishCell fish, Coordinate coord){
         super(State.FISH, coord);
         myMaxBreedTime = fish.myMaxBreedTime;
         myCurrentBreedTime = fish.myCurrentBreedTime;
     }
     public void update () {
-        myCurrentBreedTime = myCurrentBreedTime == 0 ? myMaxBreedTime : myCurrentBreedTime--;
+        myCurrentBreedTime = myCurrentBreedTime == 0 ? myMaxBreedTime : myCurrentBreedTime - 1;
     }
 
     public boolean canBreed () {
