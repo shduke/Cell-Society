4ae9319e0481a9b5440cc0b285def94dcef092e4
Sean Hudson <srh50@duke.edu>
2016-09-25 02:06:57 -0400

    added intial grid creation method and improved the step()

--- src/simulation/FireSimulation.java
+++ src/simulation/FireSimulation.java
@@ -3,14 +3,15 @@ package simulation;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import cell.Cell;
 import cell.FireCell;
 import cell.State;
 import grid.Coordinate;
+import grid.Grid;
 import grid.GridView;
 import grid.Neighbors;
-import grid.RectangleGrid;
 import grid.RectangleGridView;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
@@ -21,63 +22,61 @@ import javafx.scene.media.MediaPlayer;
 import javafx.util.Duration;
 
 public class FireSimulation extends Simulation {
-    Map<Coordinate, Cell> temp = new HashMap<Coordinate, Cell>(); //temporary solution
-    int counter = 0;
-    private boolean diagonalNeighbors;
+    Map<Coordinate, Cell> input = new HashMap<Coordinate, Cell>();   
+    
     public FireSimulation () {
-        diagonalNeighbors = true;
-        ArrayList<Cell> input = new ArrayList<Cell>();
+        
 
-        input.add(new FireCell(State.BURNING,
+        input.put(new Coordinate(2, 2), new FireCell(State.BURNING,
                                new Coordinate(2, 2)));
-        input.add(new FireCell(State.TREE, new Coordinate(3,3)));
-        setGrid(new RectangleGrid(5, 5, input));
+        //input.add(new FireCell(State.TREE, new Coordinate(3,3)));
+        generateMap();
+        setGrid(new Grid(5, 5, input));
         setGridView(new RectangleGridView(new Dimension2D(500, 500), getGrid()));
     }
+    
+    /*public void printMap() {
+        for(Cell cell : input.values()) {
+            System.out.println(cell.getMyGridCoordinate().getX() + " " + cell.getMyGridCoordinate().getY());
+        }
+    }*/
+    
+//Allows them to specify what coordinates they went, the rest are set to default by a function, gets bounds and starter map from config
+    public void generateMap() {
+       for(int r = 0; r < 5; r++) {
+           for(int c = 0; c < 5; c++) {
+               Coordinate coordinate = new Coordinate(r,c);
+               if(!input.containsKey(coordinate)) {
+                   FireCell cell = new FireCell(State.TREE, coordinate);
+                   if(r == 0 || c == 0 || r == 4 || c == 4) {
+                       cell.setMyCurrentState(State.EMPTY);
+                   }
+                   input.put(coordinate, cell);
+               }
 
+           }
+       }
+    }
+    
     //very temporary code, just here to make it work
     @Override
     public void step () {
-        counter++;
-        Iterator<Map.Entry<Coordinate, Cell>> iterCell = getGrid().getCellGridIterator();
-
-        for (Coordinate coordinate : temp.keySet()) {
-            getGrid().addCell(temp.get(coordinate));
-        }
-        temp.clear();
-        iterCell = getGrid().getCellGridIterator();
-        while (iterCell.hasNext()) {
-            Cell cell = iterCell.next().getValue();
+        //getGrid().applyFuncToCell(FireSimulation::setNextState);      HOW DO I DO THIS?
+        for(Cell cell : getGrid().getImmutableCellGrid().values()) {
             setNextState(cell);
         }
-        getGrid().updateGrid();
-        System.out.println(counter);
-        if(counter % 100 == 0)
-            this.getGridView().updateView();
-        //this.getGridView().d
+        updateGrid();
         
-    }
-
-    @Override
-    public void createNeighbors (Cell cell) {
-        Neighbors neighbors = getGrid().getNeighbors(cell, diagonalNeighbors);
-        Iterator<Coordinate> iterNewCell = neighbors.getUncreatedNeighborCoordinates();
-        while (iterNewCell.hasNext()) {
-            Coordinate coordinate = iterNewCell.next();
-            Cell newCell = new FireCell(State.EMPTY, coordinate);
-            //getGrid().addCell(newCell);
-            temp.put(coordinate, newCell);
-        }
     }
 
     // test filler code
     @Override
     public void setNextState (Cell cell) {
-        System.out.println(cell.getMyCurrentState());
-        if (cell.getMyCurrentState() == State.EMPTY) {
+        //System.out.println(cell.getMyCurrentState());
+        if (cell.getMyCurrentState().equals(State.EMPTY)) {
             cell.setMyNextState(State.TREE);
         }
-        else if (cell.getMyCurrentState() == State.TREE) {
+        else if (cell.getMyCurrentState().equals(State.TREE)) {
             cell.setMyNextState(State.BURNING);
         }
         else {
--- src/simulation/Simulation.java
+++ src/simulation/Simulation.java
@@ -9,11 +9,10 @@ import javafx.scene.shape.Shape;
 
 
 public abstract class Simulation {
-    
+    ///SimulationConfig
+    ///GridViewConfig
     private Shape myCellShape;
-    public Shape getMyCellShape () {
-        return myCellShape;
-    }
+
 
     ///these 3 fields could be put in a gridViewController
     private Grid myGrid;
@@ -29,6 +28,9 @@ public abstract class Simulation {
     
     public abstract void setNextState(Cell cell);
 
+    public Shape getMyCellShape () {
+        return myCellShape;
+    }
 
     public void setMyCellShape (Shape myCellShape) {
         this.myCellShape = myCellShape;
@@ -44,8 +46,6 @@ public abstract class Simulation {
     
     //public abstract void initializeCells(); 
 
-    public abstract void createNeighbors(Cell cell);
-
     public GridView getGridView () {
         return myGridView;
     }
@@ -58,6 +58,17 @@ public abstract class Simulation {
         root.getChildren().add(myGridView.getRoot());
     }
 
+    public void removeGridViewSceneGraph(Group root) {
+        root.getChildren().remove(myGridView.getRoot());
+    }
+    
+    ///if laggy change order
+    public void updateGrid() {
+        myGrid.updateGrid();
+        //addGridViewSceneGraph();
+        myGridView.updateview();
+        
+    }
     
     // How to go from the inputed XML ShapeType to making RectangleGrid()
 }
