ce52f70928b46fbdf7f8d0c71893fd741fbe4778
Sean Hudson <srh50@duke.edu>
2016-09-26 00:04:37 -0400

    added GameOfLifeSimulation

---
+++ src/cell/GameOfLifeCell.java
@@ -0,0 +1,11 @@
+package cell;
+
+import grid.Coordinate;
+
+public class GameOfLifeCell extends Cell{
+
+    public GameOfLifeCell (State currentState, Coordinate coordinate) {
+        super(currentState, coordinate);
+    }
+    
+}
