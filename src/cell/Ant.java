b30d3ba914d3f1de165f4b40d4e107548809d65a
Michael Schroeder <ms548@duke.edu>
2016-09-29 17:59:07 -0400

    updated ant

--- src/cell/Ant.java
+++ src/cell/Ant.java
@@ -10,6 +10,7 @@ public class Ant extends Cell {
 
     private int myLifetime;
     private boolean isMoving;
+    private String myOrientation;
     public Ant (Coordinate coordinate, int lifetime){
         
         super(State.FOODSEARCH, coordinate);
@@ -32,7 +33,7 @@ public class Ant extends Cell {
         return getMyNextState() == State.DEAD || isMoving;
     }
     
-    public ForagingAntCell move(List<ForagingAntCell> neighbors){
+    public ForagingAntCell forage(List<ForagingAntCell> neighbors){
         Iterator<ForagingAntCell> iter = neighbors.iterator();
         while(iter.hasNext()){
             if(iter.next().fullOfAnts()){
@@ -49,19 +50,70 @@ public class Ant extends Cell {
         for(ForagingAntCell cell : neighbors) {
             counter+=cell.getProb();
             if(random < counter){
-                
+                this.setOrientation(cell);
                 return cell;
             }
         } 
+        this.setOrientation(neighbors.get(0));
         return neighbors.get(0);
     }
     
+    public ForagingAntCell goHome(List<ForagingAntCell> neighbors) {
+        Iterator<ForagingAntCell> iter = neighbors.iterator();
+        while(iter.hasNext()){
+            if(iter.next().fullOfAnts()){
+                iter.remove();
+            }
+        }
+        
+        return null;
+    }
     private double getProbSum(List<ForagingAntCell> neighbors){
         double sum = 0;
         for(ForagingAntCell cell : neighbors){
             sum+=cell.getProb();
         }
         return sum;
+    }
+    
+    public void setOrientation(Cell otherCell){
+        Coordinate otherCoord = otherCell.getMyGridCoordinate();
+        Coordinate thisCoord = this.getMyGridCoordinate();
+        
+        int x = (int) (otherCoord.getX() - thisCoord.getX());
+        int y = (int)(otherCoord.getY() - thisCoord.getY());
+        
+        String first = "";
+        String second = "";
+        switch(x) {
+            case -1:
+                first = "TOP";
+                break;
+            case 0:
+                first = "MID";
+                break;
+            default:
+                first = "BOTTOM";
+                break;
+        }
+        switch(y){
+            case -1:
+                second = "LEFT";
+                break;
+            case 0:
+                second = "MID";
+                break;
+            default:
+                second = "RIGHT";
+                break;
+        }
+      
+        StringBuilder orientation = new StringBuilder();
+        
+        orientation.append(first);
+        orientation.append(second);
+        this.myOrientation = orientation.toString();
+        
     }
     
 }
