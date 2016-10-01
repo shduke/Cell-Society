8acf113a2c11091d34e771748b58f16f08dc434a
Michael Schroeder <ms548@duke.edu>
2016-09-30 23:46:56 -0400

    add diffuse and neighbor finding

--- src/cell/ForagingAntCell.java
+++ src/cell/ForagingAntCell.java
@@ -9,15 +9,25 @@ public class ForagingAntCell extends Cell {
 
     private List<Ant> myAnts;
     private List<Ant> myNextAnts;
+
     private double myFoodPheromones;
     private double myHomePheromones;
-    private double maxPheromones;
+    private double myMaxPheromones;
     private int myMaxAnts;
-    private double k;
-    private double n;
+    private double myK;
+    private double myN;
 
-    public ForagingAntCell (State myState, Coordinate myCoordinate) {
+    public ForagingAntCell (State myState,
+                            Coordinate myCoordinate,
+                            double maxPheromones,
+                            int maxAnts,
+                            double k,
+                            double n) {
         super(myState, myCoordinate);
+        myMaxPheromones = maxPheromones;
+        myMaxAnts = maxAnts;
+        myK = k;
+        myN = n;
     }
 
     public void breed (int numAnts, int lifetime) {
@@ -43,6 +53,10 @@ public class ForagingAntCell extends Cell {
         myNextAnts.clear();
     }
 
+    public void addAnt (Ant ant) {
+        myNextAnts.add(ant);
+    }
+
     private void removeDeadAnts () {
         Iterator<Ant> iter = myAnts.iterator();
         while (iter.hasNext()) {
@@ -57,11 +71,11 @@ public class ForagingAntCell extends Cell {
     }
 
     public boolean foodFull () {
-        return myFoodPheromones == maxPheromones;
+        return myFoodPheromones == myMaxPheromones;
     }
 
     public boolean homeFull () {
-        return myHomePheromones == maxPheromones;
+        return myHomePheromones == myMaxPheromones;
     }
 
     public void addPheromones (List<Cell> neighbors, boolean food) {
@@ -84,6 +98,24 @@ public class ForagingAntCell extends Cell {
         }
     }
 
+    public void spawn (int antLifetime) {
+        addAnt(new Ant(this.getMyGridCoordinate(), antLifetime));
+    }
+
+    public void diffuseAndEvaporate (List<Cell> neighbors,
+                                     double diffusionRate,
+                                     double evaporationRate) {
+        myFoodPheromones -= myFoodPheromones * evaporationRate;
+        myHomePheromones -= myHomePheromones * evaporationRate;
+        for (Cell c : neighbors) {
+            ForagingAntCell cell = (ForagingAntCell) c;
+            cell.myFoodPheromones += myFoodPheromones * diffusionRate;
+            cell.myHomePheromones += myHomePheromones * diffusionRate;
+            myHomePheromones -= myHomePheromones * diffusionRate;
+            myFoodPheromones -= myFoodPheromones * diffusionRate;
+        }
+    }
+
     public double getPheromones (boolean food) {
         return food ? myFoodPheromones : myHomePheromones;
     }
@@ -93,15 +125,15 @@ public class ForagingAntCell extends Cell {
     }
 
     public double getProb () {
-        return Math.pow(myFoodPheromones + k, n);
+        return Math.pow(myFoodPheromones + myK, myN);
     }
 
     public void setMaxPheromones (boolean food) {
         if (food) {
-            myFoodPheromones = maxPheromones;
+            myFoodPheromones = myMaxPheromones;
         }
         else {
-            myHomePheromones = maxPheromones;
+            myHomePheromones = myMaxPheromones;
         }
     }
 }
