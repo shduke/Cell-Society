76deaa5c4041897eb83494326180be2d6ce29e56
Kayla Schulz <kms114@duke.edu>
2016-10-01 19:04:20 -0400

    Added countCells to segregation simulation. Fixed bug at the beginning where it counted the cells twice

--- src/controller/SimulationController.java
+++ src/controller/SimulationController.java
@@ -17,7 +17,6 @@ public class SimulationController {
         this.simulationRoot = simulationRoot;
         File simulationConfig = new File("src/resources/Segregation.xml");
         initializeSimulation(simulationConfig.getAbsolutePath());
-        simulation.countCellsinGrid();
     }
 
     /**
